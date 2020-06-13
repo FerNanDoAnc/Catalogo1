4ab545b2cbd408d16713cb7e0c497928761c4b31
Fernando <fernando.ancajima99@gmail.com>
2020-06-13 10:27:08 -0500

    version 2.2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-;(function(){
+;
+(function() {
     //variables
-    let sticky=false
+    let sticky = false
     let currentPosition = 0
 
     const imageCounter = $("[data-name='image-counter'").attr("content")
@@ -9,56 +10,60 @@
     $("#sticky-navigation").slideUp(0)
     console.log($(window).height());
     //click pqra quq aparezca el menu responsivo
-    $("#menu-opener").on("click",function(){
-        $("#responsive-nav ul").toggleClass("active")
-        $(this).toggleClass("glyphicon-menu-hamburger")
-    })
+    $("#menu-opener").on("click", toggleNav)
+    $(".menu-link").on("click", toggleNav)
 
-    //$("menu-link").on("click",toggleNav)
+
+    function toggleNav() {
+        $("#responsive-nav ul").toggleClass("active")
+        $("#menu-opener").toggleClass("glyphicon-menu-hamburger")
+    }
 
     //Intervalos para las imagees fooda,b...
-    setInterval(()=>{
-        if(currentPosition<imageCounter){
+    setInterval(() => {
+        if (currentPosition < imageCounter) {
             currentPosition++
-        }else{
-            currentPosition=0
+        } else {
+            currentPosition = 0
         }
-        
-        
+
+
         $("#gallery .inner").css({
-            left:"-"+currentPosition*100+"%"
+            left: "-" + currentPosition * 100 + "%"
         })
-        
-    },4000)
+
+    }, 4000)
 
     //$ es similar a escribir jQuery
-    $(window).scroll(()=>{
-        const inBottom=isInBottom()
-        if (inBottom && !sticky){
+    $(window).scroll(() => {
+        const inBottom = isInBottom()
+        if (inBottom && !sticky) {
             //mostrar la navegacion sticky
             sticky = true
             stickNavigation()
         }
-        if (!inBottom && sticky){
+        if (!inBottom && sticky) {
             //ocultar la navegacion sticky
             sticky = false
             unStickNavigation()
         }
     })
 
-    function stickNavigation(){
+    function stickNavigation() {
         $("#description").addClass("fixed").removeClass("absolute")
         $("#navigation").slideUp("fast")
         $("#sticky-navigation").slideDown("fast")
     }
-    function unStickNavigation(){
+
+    function unStickNavigation() {
         $("#description").removeClass("fixed").addClass("absolute")
         $("#navigation").slideDown("fast")
         $("#sticky-navigation").slideUp("fast")
     }
-    function isInBottom(){
-        const $description=$("#description")
-        const descriptionHeight=$description.height()
-        return $(window).scrollTop() > $(window).height() - (descriptionHeight*2)
+
+    function isInBottom() {
+        const $description = $("#description")
+        const descriptionHeight = $description.height()
+        return $(window).scrollTop() > $(window).height() - (descriptionHeight * 2)
     }
 })()
